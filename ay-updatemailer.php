c9e3dc2ebbaec7df8392bb391ec47bf3ca0559e8
Erwan Guillon <erwan@ayctor.com>
2016-02-08 14:05:59 +0100

    Emails in option

--- ay-updatemailer.php
+++ ay-updatemailer.php
@@ -15,7 +15,37 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 class AyUpdateMailer{
 
   public function __construct(){
+    add_action('admin_menu', array($this, 'menu'));
     add_action('upgrader_process_complete', array($this, 'upgrade_action'), 10, 2);
+  }
+
+  public function menu(){
+    add_options_page('Update Mailer', 'Update Mailer', 'manage_options', 'updatemailer', array($this, 'settings_page'));
+  }
+
+  public function settings_page(){
+    if(isset($_POST['submit'])){
+      if(isset($_POST['um_emails'])){
+        update_option('um_emails', filter_input(INPUT_POST, 'um_emails', FILTER_SANITIZE_SPECIAL_CHARS));
+      }
+    }
+    ?>
+    <div class="wrap">
+      <h2>Update Mailer</h2>
+      <form method="post" action="options-general.php?page=updatemailer">
+        <table class="form-table">
+          <tr>
+            <th scope="row"><label for="um_emails">Emails</label></th>
+            <td>
+              <input name="um_emails" type="text" id="um_emails" value="<?php echo get_option('um_emails', ''); ?>" class="regular-text">
+              <p class="description">Enter the emails (comma separated) to receive every update report.</p>
+            </td>
+          </tr>
+        </table>
+        <p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
+      </form>
+    </div>
+    <?php
   }
 
   public function upgrade_action($upgrader_subject, $options){
@@ -38,7 +68,7 @@ class AyUpdateMailer{
     if($options['action'] == 'update' AND $options['type'] == 'core'){
 
       $subject = 'Maintenance - Votre site WordPress a été mis à jour à la version ' . $wp_version;
-      $title = 'Mise à jour de WordPess';
+      $title = 'Mise à jour de WordPress';
       $message = 'Bonjour,
       <br/><br/>Votre site WordPress vient d\'être mis à jour à la version ' . $wp_version . '.
       <br/><br/>Cordialement.
@@ -48,23 +78,26 @@ class AyUpdateMailer{
   }
 
   public function send_mail($subject, $title, $message){
-    $email = '';
+    $emails = get_option('um_emails', '');
+    if($emails != ''){
+      $email = '';
 
-    ob_start();
-    include(dirname(__FILE__) . '/mail.php');
-    $email = ob_get_contents();
+      ob_start();
+      include(dirname(__FILE__) . '/mail.php');
+      $email = ob_get_contents();
 
-    ob_end_clean();
+      ob_end_clean();
 
-    add_filter( 'wp_mail_content_type', function( $content_type ) {
-      return 'text/html';
-    });
+      add_filter( 'wp_mail_content_type', function( $content_type ) {
+        return 'text/html';
+      });
 
-    wp_mail('erwan@ayctor.com', $subject, $email);
-    
-    add_filter( 'wp_mail_content_type', function( $content_type ) {
-      return 'text/plain';
-    });
+      wp_mail('erwan@ayctor.com', $subject, $email);
+      
+      add_filter( 'wp_mail_content_type', function( $content_type ) {
+        return 'text/plain';
+      });
+    }
   }
 
 }
